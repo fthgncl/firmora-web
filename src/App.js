550efcfd2c3b910798a0799dad9a3a3279e6e7ea
fthgncl <gencal.fatih61@gmail.com>
2025-09-21 18:22:49 +0300

    React Router eklendi ve HomePage sayfası eklendi

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import './css/App.css';
+import {Route, Routes} from "react-router-dom";
+import HomePage from "./pages/HomePage";
 
 function App() {
-  return (
-    <div className="App">
-      Firmora Web Application
-    </div>
-  );
+    return (
+        <div className="page-container">
+            <Routes>
+                <Route path="/" element={<HomePage/>}/>
+            </Routes>
+        </div>
+    );
 }
 
 export default App;
