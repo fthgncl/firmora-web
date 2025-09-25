e005565f1cd88d8b4f612fd2cdc8a400b474ae19
fthgncl <gencal.fatih61@gmail.com>
2025-09-25 21:04:39 +0300

    MUI kütüphaneleri eklendi, React ve Router sürümleri güncellendi, Tema provider'ı eklendi ve BrowserRouter güncellendi.

--- src/index.js
+++ src/index.js
@@ -1,16 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './css/index.css';
-import { BrowserRouter } from 'react-router-dom';
-
+import {BrowserRouter} from 'react-router-dom';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import {ThemeProvider, createTheme} from '@mui/material/styles';
+
+const theme = createTheme();
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-    <BrowserRouter>
-        <App />
-    </BrowserRouter>
+    <ThemeProvider theme={theme}>
+        <BrowserRouter future={{v7_startTransition: true, v7_relativeSplatPath: true}}>
+            <App/>
+        </BrowserRouter>
+    </ThemeProvider>
 );
 
 reportWebVitals();
