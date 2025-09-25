ecfadf111e004e422021fd4d25065f314c69c984
fthgncl <gencal.fatih61@gmail.com>
2025-09-25 21:09:43 +0300

    MUI AppBar bileşeni eklendi ve tüm sayfalara entegre edildi

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import './css/App.css';
 import {Route, Routes} from "react-router-dom";
+import AppBar from "./components/AppBar";
 import HomePage from "./pages/HomePage";
 import NotFound from "./pages/NotFound";
 
 function App() {
     return (
         <div className="page-container">
+            <AppBar/>
             <Routes>
                 <Route path="/" element={<HomePage/>}/>
                 <Route path="*" element={<NotFound/>}/>
