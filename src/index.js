27f19e80e38bb57f06f529f7fcc36c14e21f4c82
fthgncl <gencal.fatih61@gmail.com>
2025-09-27 00:17:49 +0300

    ThemeProvider kaldırıldı: index.js dosyasındaki gereksiz ThemeProvider bileşeni kaldırıldı.

--- src/index.js
+++ src/index.js
@@ -4,22 +4,17 @@ import './css/index.css';
 import {BrowserRouter} from 'react-router-dom';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import {ThemeProvider, createTheme} from '@mui/material/styles';
 import {AuthProvider} from './contexts/AuthContext';
 import {AppThemeProvider} from './contexts/ThemeContext';
 import './services/i18n';
-
-const theme = createTheme();
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
     <AuthProvider>
         <AppThemeProvider>
-            <ThemeProvider theme={theme}>
-                <BrowserRouter future={{v7_startTransition: true, v7_relativeSplatPath: true}}>
-                    <App/>
-                </BrowserRouter>
-            </ThemeProvider>
+            <BrowserRouter future={{v7_startTransition: true, v7_relativeSplatPath: true}}>
+                <App/>
+            </BrowserRouter>
         </AppThemeProvider>
     </AuthProvider>
 );
