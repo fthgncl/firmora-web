3d1b56051baddbd06cd21fe6b43512ff2f4fd67c
fthgncl <gencal.fatih61@gmail.com>
2025-09-27 00:59:37 +0300

    Axios interceptor eklendi: Tüm isteklere dil header'ı eklenerek dil değişikliği durumunda güncellenmesi sağlandı.

--- src/services/i18n.js
+++ src/services/i18n.js
@@ -1,6 +1,7 @@
 import i18n from "i18next";
 import { initReactI18next } from "react-i18next";
 import LanguageDetector from 'i18next-browser-languagedetector';
+import axios from 'axios';
 
 // Locales klasöründeki tüm JSON dosyalarını dinamik olarak yükle
 const loadLocales = () => {
@@ -26,5 +27,21 @@ i18n
         fallbackLng: "en",
         interpolation: { escapeValue: false },
     });
+
+// Axios interceptor ile tüm isteklere dil header'ını ekle
+axios.interceptors.request.use((config) => {
+    config.headers['Accept-Language'] = i18n.language;
+    return config;
+});
+
+// Dil değiştiğinde interceptor'ı güncelle
+i18n.on('languageChanged', (lng) => {
+    // Mevcut interceptor'ları temizle ve yeniden ekle
+    axios.interceptors.request.clear();
+    axios.interceptors.request.use((config) => {
+        config.headers['Accept-Language'] = lng;
+        return config;
+    });
+});
 
 export default i18n;
