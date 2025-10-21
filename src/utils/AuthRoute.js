b11e803a56a27dedf807a5eaf12a3d46ceb6d2c2
fthgncl <gencal.fatih61@gmail.com>
2025-10-21 03:40:40 +0300

    Gereksiz console.log komutu kaldırıldı

--- src/utils/AuthRoute.js
+++ src/utils/AuthRoute.js
@@ -39,7 +39,6 @@ const AuthRoute = ({
                     companyId,
                     requireRoles
                 );
-                console.log('permissionResult',permissionResult);
                 setHasPermission(permissionResult);
             } catch {
                 setHasPermission(false);
